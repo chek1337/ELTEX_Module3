61b2839c041dacb3283a0c09c5b695e6fc609239
chek1337 <91788990+chek1337@users.noreply.github.com>
2024-04-15 21:43:23 +0700

    Выполнено задание 2.5

---
+++ 2.5/sigstopProg.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <wait.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        fprintf(stderr, "Wrong number of args!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t pid = atoi(argv[1]);
+
+    kill(pid, SIGSTOP);
+    exit(EXIT_SUCCESS);
+}
